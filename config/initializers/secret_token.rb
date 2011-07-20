bd2fe20aefb95fdbde0e781f52792cf916953747
rbeltran <rbeltran@us001ec94c4941.am.sony.com>
2011-07-20 13:48:27 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Word::Application.config.secret_token = '910f1db5c2ef891969151b1ee8c4c00e9055d0d714ace19dee3d6fa49893d33d799ff6015a711ac00a29e6bb2191045c02efb358e5caaae88339e8048fc9948c'
