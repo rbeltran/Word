a4f41f178c994e2b076b67eee8d9b3ce7bd8891a
rbeltran <rbeltran@us001ec94c4941.am.sony.com>
2011-07-28 15:16:38 -0700

    updated WWF dictionary with new updates

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,4 +1,4 @@
-class Dictionary < ActiveRecord::Base
+class Dictionary
 
 	$WORDSMITH_FILE = "doc/yawl.txt"
 	$WORDS_WITH_FRIENDS_FILE = "doc/enable1.txt"
@@ -12,8 +12,20 @@ class Dictionary < ActiveRecord::Base
 		$wordSmithHash = contents.split( /[,\s]+/ )
 		contents = File.open( $WORDS_WITH_FRIENDS_FILE, "r" ).read
 		$wordsWithFriendsHash = contents.split( /[,\s]+/ )
-		$wordsWithFriendsHash << "zen"
-		$wordsWithFriendsHash << "texting"
+		updateWWF
+	end
+	
+	def updateWWF
+		deletedWords = File.open( "doc/wwf-deleted.txt", "r" ).read
+		deletedWords2 = File.open( "doc/wwf-deleted_4_02.txt", "r" ).read
+		deletedArray = deletedWords.split( /[,\s]+/ ).map{ |element| element.downcase } 
+		deletedArray2 = deletedWords2.split( /[,\s]+/ ).map{ |element| element.downcase }
+		 puts "WWF size = " + $wordsWithFriendsHash.length.to_s
+		$wordsWithFriendsHash = $wordsWithFriendsHash - ( deletedArray + deletedArray2 )
+		puts "WWF size = " << $wordsWithFriendsHash.length.to_s
+		added = File.open( "doc/wwf-added.txt", "r" ).read
+		$wordsWithFriendsHash = $wordsWithFriendsHash + added.split( /[,\s]+/ ).map{ |item| item.downcase }
+		puts "WWF size = " + $wordsWithFriendsHash.length.to_s
 	end
 	
 	def buildDictionary
